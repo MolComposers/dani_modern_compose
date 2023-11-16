fca00ad4c9392b9c7a4e7785028e50c114ac7400
Dani43 <eldms0406@naver.com>
2023-11-16 16:40:37 +0900

    실습:Column, Row, Text

--- app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
@@ -3,7 +3,18 @@ package com.example.modernprograming_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.modernprograming_jetpackcompose.ui.theme.ModernPrograming_JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +22,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ModernPrograming_JetpackComposeTheme {
-                Surface() {
-                    
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = Color.Gray)
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        Text("hello")
+                        Text(text = "World")
+                    }
                 }
             }
         }
