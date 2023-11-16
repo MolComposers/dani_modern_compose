af4ebd14e4482d74a72ae25898450de42b1fc3fa
Dani43 <eldms0406@naver.com>
2023-11-16 16:56:25 +0900

    실습: Box

--- app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.modernprograming_jetpackcompose.ui.theme.ModernPrograming_JetpackComposeTheme
 
@@ -21,20 +22,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ModernPrograming_JetpackComposeTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Gray)
-                            .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        Text("hello")
-                        Text(text = "World")
-                    }
-                }
+            Box(
+                modifier = Modifier
+                    .background(color = Color.Blue)
+                    .fillMaxWidth()
+                    .height(200.dp)
+            ) {
+                Text("Hello")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ModernPrograming_JetpackComposeTheme {
+        Box(
+            modifier = Modifier
+                .background(color = Color.Blue)
+                .fillMaxWidth()
+                .height(200.dp)
+        ) {
+            Text("Hello")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Text("World")
             }
         }
     }
