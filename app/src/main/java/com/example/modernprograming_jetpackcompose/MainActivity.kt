fbc9e46314bc4c144f825ed02ba8e6caeef962b4
Dani43 <eldms0406@naver.com>
2023-11-16 17:00:19 +0900

    실습: Column으로 List 만들기

--- app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
@@ -4,31 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.modernprograming_jetpackcompose.ui.theme.ModernPrograming_JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
+            val scrollState = rememberScrollState()
+            Column(
                 modifier = Modifier
                     .background(color = Color.Blue)
                     .fillMaxWidth()
-                    .height(200.dp)
+                    .verticalScroll(scrollState)
             ) {
-                Text("Hello")
+                for (i in 1..50) {
+                    Text("글씨 $i")
+                }
             }
         }
     }
@@ -37,22 +38,4 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ModernPrograming_JetpackComposeTheme {
-        Box(
-            modifier = Modifier
-                .background(color = Color.Blue)
-                .fillMaxWidth()
-                .height(200.dp)
-        ) {
-            Text("Hello")
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                contentAlignment = Alignment.BottomEnd
-            ) {
-                Text("World")
-            }
-        }
-    }
 }
