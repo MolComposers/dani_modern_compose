7e90cb569f5982a380526e27bdb9b65e2dc6c2a0
Dani43 <eldms0406@naver.com>
2023-11-17 23:03:00 +0900

    실습: Scaffold, TextField, Button, 구조분해, SnackBar, 코루틴 스코프: 버튼 클릭 시 키보드 내려가게 하기

--- app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/modernprograming_jetpackcompose/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -18,20 +19,28 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,21 +48,37 @@ class MainActivity : ComponentActivity() {
                 mutableStateOf("")
             }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                TextField(
-                    value = text,
-                    onValueChange = setValue)
-                            Button (onClick = {
+            val snackBarHostState = remember { SnackbarHostState() }
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
 
-                            }) {
+            Scaffold(
+                snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
+            ) { contentPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(contentPadding),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue
+                    )
+                    Button(onClick = {
+                        keyboardController?.hide()
+                        scope.launch {
+                            snackBarHostState.showSnackbar(
+                                "hello $text"
+                            )
+                        }
+                    }) {
                         Text(text = "클릭!!")
                     }
+                }
             }
+
         }
     }
 }
